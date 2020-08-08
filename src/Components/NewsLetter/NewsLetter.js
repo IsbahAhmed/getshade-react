19bc3fa1fb0bf9401f99d546ae8e38147d51f13f
IsbahAhmed <isbahahmed90@gmail.com>
2020-08-08 21:00:20 +0500

    newsletter ready

--- src/Components/NewsLetter/NewsLetter.js
+++ src/Components/NewsLetter/NewsLetter.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react'
 import StyledInput from '../StyledInput/StyledInput'
-import "./NewsLetter.css"
 
+import "./NewsLetter.css"
+import Paragraph from "../Paragraph/Paragraph"
 import Button from '../Button/Button'
 import Heading from '../Heading/Heading'
+import { firestore } from '../../Firebase/firebase'
 const NewsLetter = () => {
+  const [statusMsg,setStatusMsg] = useState({
+    msg:"",error:""
+  })
     var [inputValue,setValue] = useState("")
-   
+   var handleSubmit = async (e)=>{
+     e.preventDefault();
+    if(inputValue){
+
+      try{
+        await firestore.collection('newsletter_subcriptions').doc(inputValue).set({email:inputValue})
+        setStatusMsg({msg:"Successfully subscribed to newsletter!"})
+      }catch(error){
+        console.log(error.message)
+        setStatusMsg({error:"An error occured!"})
+
+      }
+    }
+   }
     return (
         <div className="newsletter">
             <div className="text-part">
@@ -16,7 +34,7 @@ const NewsLetter = () => {
             </div>
   
         <div className="form-part">
-      <form className="input-xs" >
+      <form className="input-xs" onSubmit={handleSubmit}>
      
                  <div className="">
                  <div className="xs1">
@@ -36,8 +54,21 @@ const NewsLetter = () => {
                     style={{color:"black",paddingBottom:"1rem",border:"none",width:"max-content",fontWeight:"600",letterSpacing:"3px"}}
                      backgroundColor="transparent"/>
                    </div>
+                   
                  </div>
-              
+           {
+             statusMsg.msg ?    <Paragraph style={{textAlign:"start",marginTop:"1rem",color:"var(--green)"}}>
+            {
+              statusMsg.msg
+            }
+           </Paragraph>
+           : statusMsg.error ?    <Paragraph style={{textAlign:"start",marginTop:"1rem",color:"var(--red)"}}>
+           {
+             statusMsg.error
+           }
+         </Paragraph>
+         : ""
+           }
       </form>
             </div>
     
